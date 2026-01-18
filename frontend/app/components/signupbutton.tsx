4cf9f3b77491d9a658942046dd76c38860d0ae1c
Richard Jr Rojo <chad@Richards-MacBook-Air-7.local>
2026-01-19 00:34:48 +0800

    frontend optimization

--- frontend/app/components/signupbutton.tsx
+++ frontend/app/components/signupbutton.tsx
@@ -1,7 +1,8 @@
 export default function SignupButton(){
     return (
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-neutral w-25 rounded-full">
             Signup
         </button>
     )
 }
+
--- frontend/app/services/api.tsx
+++ frontend/app/services/api.tsx
@@ -1,7 +1,8 @@
 
 export async function getLandingPageData(){
     const res = await fetch("http://127.0.0.1:8000/",{
-        method:"GET"
+        method:"GET",
+        cache: "force-cache"
     })
     const data = await res.json()
     return data 
