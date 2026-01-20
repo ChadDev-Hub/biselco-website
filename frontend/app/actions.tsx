d78619d36c38317c20d8b65461fe85b60e1e7b6f
Richard Jr Rojo <chad@Richards-MacBook-Air-7.local>
2026-01-20 23:38:21 +0800

    Finishing Authentication, Signup and Login backend and frontend

--- frontend/app/actions.tsx
+++ frontend/app/actions.tsx
@@ -1,36 +1,52 @@
 "use server";
 const baseUrl = process.env.BASESERVERURL
-const Signup = async (formdata:FormData) => {
-    const data =  formdata
+const Signup = async (formdata: FormData) => {
+    const data = formdata
     const res = await fetch(`${baseUrl}/auth/signup`,
         {
-            method:"POST",
-            body:data
+            method: "POST",
+            body: data
         }
     )
-    if (!res.ok){
+    if (!res.ok) {
         const results = await res.json()
         throw results.detail
     }
 
     const results = await res.json()
     return results
-    
+
 }
 
 
 
 
-const Login = async(formdata:FormData) => {
-    const data = formdata
-    const res = await fetch(`${baseUrl}/auth/login`,
-        {
-            method: "POST",
-            body: data
+const Login = async (formdata: FormData) => {
+    const body = new URLSearchParams({
+        username: formdata.get("username") as string,
+        password: formdata.get("password") as string
+    }).toString();
+    try {
+        const res = await fetch(`${baseUrl}/auth/token`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: body,
+            }
+        )
+        if (!res.ok){
+            console.log(await res.json())
+        }else{
+            return await res.json()
         }
         
-    )
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 
-export {Signup, Login};
+export { Signup, Login };
