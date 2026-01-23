04ac08976c81f3301b3f70333e721970c38d927e
Richard Rojo <richardrojo61@gmail.com>
2026-01-23 18:58:52 +0800

    Frontend Optimization

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,19 @@
 "use server"
-import { getLandingPageData } from "./services/api";
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
-import Hero from "./components/hero";
 
+import { redirect } from "next/navigation";
+import { getNewsPage } from "./services/api";
 export default async function Home() {
-  const accessToken = (await cookies()).get("access_token")
-  if (accessToken){
-    redirect("/news")
+  const res = await getNewsPage()
+  if (res.error){
+    redirect("/landing")
   }
-  const landingPageData = await getLandingPageData()
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen  w-full flex-col items-center justify-between">
 
         {
-          <Hero
-            subtitle={landingPageData.hero.subtitle}
-            description={landingPageData.hero.description} />
+          res.news
         }
       </main>
     </div>
