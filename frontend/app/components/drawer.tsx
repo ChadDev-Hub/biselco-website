6bee52c04162ee6ef32cf591875fd100fc8fca06
RICHARD ROJO JR <richardrojo61@gmail.com>
2026-01-27 07:13:17 +0800

    News page

--- frontend/app/components/drawer.tsx
+++ frontend/app/components/drawer.tsx
@@ -1,25 +1,34 @@
-
+"use client"
 import React from 'react'
 import NavBar from './navbar'
+import { usePathname } from 'next/navigation'
+import { Logout } from "../services/clientApi"
+import { useRouter } from 'next/navigation'
 type Props = {
-    children:React.ReactNode
+    children: React.ReactNode;
+    baseurl?: string;
+    title?: string
 }
 
-const Drawer = ({children}: Props) => {
+const Drawer = ({ children, baseurl , title }: Props) => {
+    const currentRoute = usePathname()
+    const router = useRouter()
+    const handleLogout = () => {
+        Logout(baseurl),
+            router.push("/landing")
+    }
     return (
         <div className="drawer absolute z-40 lg:drawer-open">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Navbar */}
-                <NavBar/>
+                <NavBar title={title} />
                 {/* Page content here */}
                 <div className="lg:p-2">
                     {children}
                 </div>
             </div>
-
-
-            <div className="drawer-side is-drawer-close:overflow-visible">
+            <div className={`drawer-side is-drawer-close:overflow-visible mt-15 fixed ${currentRoute === "/landing" ? "hidden" : "visible"}`} >
                 <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                 <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
                     {/* Sidebar content here */}
@@ -35,10 +44,17 @@ const Drawer = ({children}: Props) => {
 
                         {/* List item */}
                         <li>
+
                             <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
                                 {/* Settings icon */}
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
                                 <span className="is-drawer-close:hidden">Settings</span>
+                            </button>
+                        </li>
+                        <li>
+                            <button  onClick={handleLogout} type="button" className="is-drawer-close:tooltip is-drawer-close:tooltip-right">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
+                                <span className='is-drawer-close:hidden'>Logout</span>
                             </button>
                         </li>
                     </ul>
--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useEffect, useState } from "react"
 import Image from "next/image"
-import { Logout } from "../services/clientApi"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 interface Props {
     title?: string,
-    baseurl?: string;
+
 }
-export default function NavBar({baseurl, title }: Props) {
+export default function NavBar({ title }: Props) {
     const router = useRouter()
+    const currentRouter = usePathname()
     const [currentTheme, setTheme] = useState(() => {
         if (typeof window == "undefined") {
             return "cupcake"
@@ -42,37 +42,34 @@ export default function NavBar({baseurl, title }: Props) {
 
 
     // LOGOUT
-    const handleLogout = () =>{
-        Logout(baseurl),
-        router.push("/landing")
-    }
+
     return (
-        <div className="navbar w-full fixed pr-20">
-            <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                        {/* Sidebar toggle icon */}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
-                    </label>
+        <div className="navbar  top-0 left-0 right-0 z-50 fixed p-4 bg-base-200/45 backdrop-blur-sm ">
+            <label htmlFor="my-drawer-4" aria-label="open sidebar" className={`btn btn-square btn-ghost md:inline-flex hidden ${currentRouter === "/landing"? "hidden" : "visible"}`}>
+                {/* Sidebar toggle icon */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+            </label>
             <Image
                 loading="eager"
                 src="/biselco-icon.png"
                 alt="biselco"
-                width={20}
-                height={20}
-                />
+                width={30}
+                height={30}
+            />
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl text-blue-700">{title}</a>
             </div>
             <div className="flex flex-none z-40">
-                    <button type="button" aria-label="Search" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
-                    </button>
-                    <button type="button" aria-label="Notification" className="btn btn-ghost btn-circle">
-                        <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-                            <span className="badge badge-xs badge-primary indicator-item"></span>
-                        </div>
-                    </button>
-           
+                <button type="button" aria-label="Search" className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+                </button>
+                <button type="button" aria-label="Notification" className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
+                    </div>
+                </button>
+
                 <div className="dropdown dropdown-end z-20">
                     <div aria-label="Profile" tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
@@ -95,7 +92,7 @@ export default function NavBar({baseurl, title }: Props) {
                             </a>
                         </li>
                         <li><a>Settings</a></li>
-                        <li><button type="button" onClick={handleLogout} className="btn btn-neutral">Logout</button></li>
+                        <li></li>
                     </ul>
                 </div>
             </div>
