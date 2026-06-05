af1b3464c40e3b96c74bbb34e39c3d511c7fdebc
Chad <richardrojo61@gmail.com>
2026-06-05 20:19:43 +0800

    fixing Fab

--- frontend/app/(protected)/agma-spin-wheel/components/spin-navigation.tsx
+++ frontend/app/(protected)/agma-spin-wheel/components/spin-navigation.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const SpinNavigation = () => {
   return (
-    <div className="fab sticky right-1 bottom-8">
+    <div className="fab fixed right-8 bottom-20 z-50">
         <Link href="/agma-spin-wheel" className="btn btn-primary btn-circle">
             <LoaderPinwheel className="animate-spin" />
         </Link>
